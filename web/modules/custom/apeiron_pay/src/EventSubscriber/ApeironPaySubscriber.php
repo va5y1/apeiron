2e1d4aab42674b9355247315b6e8aff62b2aa832
Cable <v.kydyba@drudesk.com>
2020-05-03 18:49:40 +0300

    Custom Editor start

---
+++ web/modules/custom/apeiron_pay/src/EventSubscriber/ApeironPaySubscriber.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\apeiron_pay\EventSubscriber;
+
+use Drupal\commerce_order\Event\OrderEvent;
+use Drupal\Core\Messenger\MessengerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * apeiron_pay event subscriber.
+ */
+class ApeironPaySubscriber implements EventSubscriberInterface {
+
+  /**
+   * The messenger.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * Constructs event subscriber.
+   *
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger.
+   */
+  public function __construct(MessengerInterface $messenger) {
+    $this->messenger = $messenger;
+  }
+
+  /**
+   * Kernel request event handler.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
+   *   Response event.
+   */
+  public function onKernelRequest(GetResponseEvent $event) {
+    $this->messenger->addStatus(__FUNCTION__);
+  }
+
+  /**
+   * Kernel response event handler.
+   *
+   * @param OrderEvent $event
+   */
+  public function onPaid(OrderEvent $event) {
+    $order = $event->getOrder();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      KernelEvents::REQUEST => ['onKernelRequest'],
+      'commerce_order.order.paid' => 'onPaid',
+    ];
+  }
+
+}
